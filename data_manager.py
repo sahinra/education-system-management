8127f9e990055ee615c7909b16f360f9d3ebfe7f
Rana <sahinra222@gmail.com>
2024-01-19 14:23:39 +0100

    A few endpoints were implemented

---
+++ data_manager.py
@@ -0,0 +1,41 @@
+import db_connection
+
+
+@db_connection.handle_connection
+def get_all_courses(cursor):
+    cursor.execute("""
+        SELECT * FROM courses
+    """)
+    result = cursor.fetchall()
+    return result
+
+
+@db_connection.handle_connection
+def get_all_subjects_details_by_student_id(cursor, student_id):
+    cursor.execute(f"""
+        SELECT s.student_id, cr.course_id, cr.title as course_title, sb.subject_id, sb.title as subject_title, 
+            t.teacher_id, t.teacher_name, t.teacher_surname
+        FROM students s
+        JOIN student_groups sg ON s.group_id = sg.group_id
+        JOIN groups_courses_lookup gcl ON sg.group_id = gcl.group_id
+        JOIN courses cr ON gcl.course_id = cr.course_id
+        JOIN subjects sb ON cr.subject_id = sb.subject_id
+        JOIN teachers t ON sb.subject_id = t.subject_id AND sg.group_id = t.group_id
+        WHERE s.student_id = {student_id};
+    """)
+    result = cursor.fetchall()
+    return result
+
+
+@db_connection.handle_connection
+def get_all_students_by_teacher_id(cursor, teacher_id):
+    cursor.execute(f"""
+        select t.teacher_id, t.teacher_name, t.teacher_surname, sg.group_name, s.student_id, 
+        s.student_name, s.student_surname from teachers t
+        JOIN teachers_groups_lookup tgl ON tgl.teacher_id = t.teacher_id
+        join student_groups sg on sg.group_id = tgl.group_id
+        join students s on s.group_id = sg.group_id
+        where t.teacher_id = {teacher_id}
+    """)
+    result = cursor.fetchall()
+    return result
--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+import data_manager
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def get_all_courses():
+    result = data_manager.get_all_courses()
+    return result
+
+
+# User (student) can see all the subjects on the course they are enrolled on, along with who teaches them.
+# Ex: id = 3
+@app.route("/students/<int:id>/subjects_details", methods=["GET"])
+def get_all_subjects_details_by_student_id(id):
+    result = data_manager.get_all_subjects_details_by_student_id(id)
+    return result
+
+
+# User (student) can see their exams (upcoming, failed, passed).
+# User (teacher) can see all the students they teach.
+# Ex: id = 1
+@app.route("/teachers/<int:id>/students", methods=["GET"])
+def get_all_students_by_teacher_id(id):
+    result = data_manager.get_all_students_by_teacher_id(id)
+    return result
+
+# User (teacher) can add exams and assign subjects to them. The status of an exam can be changed.
+# User (administrator) can add courses and assign subjects to them.
+# User (administrator) can add students, student groups, teachers, subjects, and make the connections between them.
+
+
 if __name__ == "__main__":
-    print("hello")
+    app.run(debug=True)
